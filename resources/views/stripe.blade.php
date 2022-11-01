421afacaaf2e26a9342843bd15ec309422e553f6
Majidshah83 <shahmajid508@gmail.com>
2022-11-01 22:11:17 +0500

    update

--- resources/views/stripe.blade.php
+++ resources/views/stripe.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel 8 - Stripe Payment Gateway Integration Example - ItSolutionStuff.com</title>
+    <title>Stripe Payment Gateway Integration</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <style type="text/css">
@@ -26,7 +26,7 @@
 
 <div class="container">
 
-    <h1>Laravel 8 - Stripe Payment Gateway Integration Example <br/> ItSolutionStuff.com</h1>
+    <h1>Stripe Payment Gateway Integration</h1>
 
     <div class="row">
         <div class="col-md-6 col-md-offset-3">
@@ -35,7 +35,7 @@
                     <div class="row display-tr" >
                         <h3 class="panel-title display-td" >Payment Details</h3>
                         <div class="display-td" >
-                            <img class="img-responsive pull-right" src="http://i76.imgup.net/accepted_c22e0.png">
+                            <img class="img-responsive pull-right" src="{{url('http://i76.imgup.net/accepted_c22e0.png')}}">
                         </div>
                     </div>
                 </div>
